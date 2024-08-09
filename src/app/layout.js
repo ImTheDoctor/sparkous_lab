529ad21808198b0ba0dd4291bcff89c778c19696
ImTheDoctor <henrikhayrapetyan91@gmail.com>
2024-08-09 18:51:07 +0400

    add speedinsight and responsive

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import { Spline_Sans } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./globals.css";
 
 const spline_sans = Spline_Sans({ subsets: ["latin"] });
@@ -10,7 +11,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={spline_sans.className}>{children}</body>
+      <body className={spline_sans.className}>
+        {children}
+        <SpeedInsights />
+      </body>
     </html>
   );
 }
